97664e6447a660c7f521dd4d5095320ee8430e1c
monirul-fahad <immonirulfahad@gmail.com>
2022-01-28 21:35:30 +0600

    deleted blog

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ async function run() {
     app.delete("/products/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
-      const result = await productCollection.deleteOne(query);
+      const result = await blogCollection.deleteOne(query);
       res.json(result);
     });
   } finally {
