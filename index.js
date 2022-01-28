375760a1f32b9c5a6db5e1883313aaad83b177e9
monirul-fahad <immonirulfahad@gmail.com>
2022-01-28 21:21:16 +0600

    deleted api

--- index.js
+++ index.js
@@ -66,6 +66,14 @@ async function run() {
       }
       res.json({ admin: isAdmin });
     });
+
+    // DELETE product API
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.json(result);
+    });
   } finally {
     //  await client.close()
   }
